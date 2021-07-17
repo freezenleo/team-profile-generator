3e9ff118220af0d15bf0bd117c37e37079219392
Clark <81452611+freezenleo@users.noreply.github.com>
2021-07-17 08:24:11 -0500

    creating html

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const inquirer = require('inquirer');
-
 const fs = require('fs');
-const generatePage = require('./src/page-template');
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const generatePage = require('./src/page-template.js');
+
+const profileData = [];
 
 //ask for basic info
 const promptUser = () => {
@@ -10,13 +15,13 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of team manager? (Required)',
+            message: 'What is the name of employee? (Required)',
             validate: nameInput => {
                 if (nameInput) {
                     return true;
                 }
                 else {
-                    console.log('Please enter manager name!');
+                    console.log('Please enter name!');
                     return false;
                 }
             }
@@ -50,188 +55,142 @@ const promptUser = () => {
             }
         },
         {
-            type: 'input',
-            name: 'officeNumber',
-            message: 'What is your office number? (Required)',
-            validate: officeNumberInput => {
-                if (officeNumberInput) {
-                    return true;
-                }
-                else {
-                    console.log('Please enter your office number!');
-                    return false;
-                }
-            }
-        }
-    ])
-}
-
-//ask for role question option
-const promptRole = profileData => {
-    if (!profileData.employee) {
-        profileData.employee = [];
-    }
-
-    console.log(`
-    =================
-    Add New Employee
-    =================
-    `);
-
-    return inquirer.prompt([
-        {
             type: 'list',
             name: 'role',
-            message: 'What do you want to add?',
-            choices: ['Engineer', 'Intern', 'Finish building my team']
+            message: 'What is your role?',
+            choices: ['Manager', 'Engineer', 'Intern']
         },
     ])
-        .then(employeeData => {
-            profileData.employee.push(employeeData);
-            if (employeeData.role === 'Engineer') {
-                return promptEngineer();
+        .then(function ({ name, id, email, role }) {
+            let roleSection = '';
+            if (role === 'Manager') {
+                roleSection = 'officeName';
+            }
+            else if (role === 'Engineer') {
+                roleSection = 'username';
 
             }
-            else if (employeeData.role === 'Intern') {
-                return promptIntern();
+            else if (role === 'Intern') {
+                roleSection = 'school';
             }
-            else {
-                return profileData;
-            }
+            inquirer.prompt([
+                {
+                    message: `What is your ${roleSection}?`,
+                    name: "roleSection",
+                },
+                {
+                    type: "list",
+                    name: "addMembers",
+                    message: "Would you like to add more team members?",
+                    choices: [
+                        "Yes",
+                        "No"
+                    ],
+                }
+            ])
+                .then(function ({ roleSection, addMembers }) {
+                    let newMember = '';
+                    if (roleSection === 'Manager') {
+                        newMember = new Manager(name, id, email, role, roleSection);
+                    }
+                    else if (roleSection === 'Engineer') {
+                        newMember = new Engineer(name, id, email, role, roleSection);
+                    }
+                    else if (roleSection === 'Engineer') {
+                        newMember = new Intern(name, id, email, role, roleSection)
+                    }
+                    profileData.push(newMember)
+                    // .then(profileData => {
+                    if (addMembers === "Yes") {
+                        promptUser(profileData);
+                    }
+                    else {
+                        return profileData;
+                    }
+                    // })
+                })
+
         })
 }
 
-//as engineer role question
-const promptEngineer = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of engineer? (Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                }
-                else {
-                    console.log('Please enter engineer name!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is the engineer id number? (Required)',
-            validate: idInput => {
-                if (idInput) {
-                    return true;
-                }
-                else {
-                    console.log('Please enter your id number!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'eamil',
-            message: 'What is engineer email? (Required)',
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                }
-                else {
-                    console.log('Please enter your email!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'username',
-            message: 'What is engineer GitHub username? (Required)',
-            validate: usernameInput => {
-                if (usernameInput) {
-                    return true;
-                }
-                else {
-                    console.log('Please enter your GitHub username!');
-                    return false;
-                }
-            }
-        },
-    ])
-        .then(promptRole);
-}
-
-//as intern role question
-const promptIntern = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of intern? (Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                }
-                else {
-                    console.log('Please enter intern name!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is the intern id number? (Required)',
-            validate: idInput => {
-                if (idInput) {
-                    return true;
-                }
-                else {
-                    console.log('Please enter your id number!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'eamil',
-            message: 'What is the intern email? (Required)',
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                }
-                else {
-                    console.log('Please enter your email!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: 'What is the intern school? (Required)',
-            validate: schoolInput => {
-                if (schoolInput) {
-                    return true;
-                }
-                else {
-                    console.log('Please enter your school!');
-                    return false;
-                }
-            }
-        },
-    ])
-        .then(promptRole);
-}
-
-
 
 promptUser()
-    .then(promptRole)
     .then(profileData => {
-        return generatePage(profileData);
+        const pageHTML = generatePage(profileData);
+
+        fs.writeFile('./dist/index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+            console.log('Page created in dist folder!')
+        })
+
     })
-    // .then(writefile)
+    // .catch(err => {
+    //     console.log(err);
+    // });
+
+
+    // as engineer role question
+// const promptManager = () => {
+//     return inquirer.prompt([
+//         {
+//             type: 'input',
+//             name: 'officeNumber',
+//             message: 'What is your office number? (Required)',
+//             validate: officeNumberInput => {
+//                 if (officeNumberInput) {
+//                     return true;
+//                 }
+//                 else {
+//                     console.log('Please enter your office number!');
+//                     return false;
+//                 }
+//             }
+//         },
+
+
+//     ])
+
+// }
+
+
+// //as engineer role question
+// const promptEngineer = () => {
+//     return inquirer.prompt([
+//         {
+//             type: 'input',
+//             name: 'username',
+//             message: 'What is engineer GitHub username? (Required)',
+//             validate: usernameInput => {
+//                 if (usernameInput) {
+//                     return true;
+//                 }
+//                 else {
+//                     console.log('Please enter your GitHub username!');
+//                     return false;
+//                 }
+//             }
+//         },
+//     ])
+
+// }
+
+// //as intern role question
+// const promptIntern = () => {
+//     return inquirer.prompt([
+//         {
+//             type: 'input',
+//             name: 'school',
+//             message: 'What is the intern school? (Required)',
+//             validate: schoolInput => {
+//                 if (schoolInput) {
+//                     return true;
+//                 }
+//                 else {
+//                     console.log('Please enter your school!');
+//                     return false;
+//                 }
+//             }
+//         },
+//     ])
+
+// }
--- src/page-template.js
+++ src/page-template.js
@@ -1,6 +1,88 @@
+//create engineer section
+const generateEngineer = teamArr => {
 
+    return `
+    <div class="card mx-auto mb-3" style="width: 18rem;">
+        ${teamArr
+            .filter(({ role }) => 'Engineer')
+            .map(({ name, id, email, role, roleSection }) => {
+                return `  
+            <div class="card mx-auto mb-3" style="width: 18rem;">
+            <h5 class="card-header">${name}</br>${role}</h5>            
+            <ul class="list-group list-groupflush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">
+                    <a href="mailto:${email}">Email: ${email}</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="https://github.com/${roleSection}">GitHub: ${roleSection}</a>
+                </li>
+            </div>
+        </div>
+    </div>
+    `;
+            })
+            .join('')
+        }
+        ${teamArr
+            .filter(({ role }) => 'Intern')
+            .map(({ name, id, email, role, roleSection }) => {
+                return `  
+            <div class="card mx-auto mb-3" style="width: 18rem;">
+            <h5 class="card-header">${name}</br>${role}</h5>            
+            <ul class="list-group list-groupflush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">
+                    <a href="mailto:${email}">Email: ${email}</a>
+                </li>
+                <li class="list-group-item">
+                   School: ${roleSection}
+                </li>
+            </div>
+        </div>
+    </div>
+    `;
+            })
+            .join('')
+        }
 
-const generatePage = () => {
+    ${teamArr
+            .filter(({ role }) => 'Engineer')
+            .map(({ name, id, email, role, roleSection }) => {
+                return `  
+            <div class="card mx-auto mb-3" style="width: 18rem;">
+            <h5 class="card-header">${name}</br>${role}</h5>            
+            <ul class="list-group list-groupflush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">
+                    <a href="mailto:${email}">Email: ${email}</a>
+                </li>
+                <li class="list-group-item">
+                    Office: ${roleSection}
+                </li>
+            </div>
+        </div>
+    </div>
+    `;
+            })
+            .join('')
+        }
+
+    `
+}
+
+// create intern section 
+const generateIntern = internSection => {
+    if (!internSection) {
+        return ``;
+    }
+
+    return `
+    <
+    `
+}
+
+const generatePage = profileArr => {
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -11,20 +93,26 @@ const generatePage = () => {
         <title>Team Profile</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
         <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="style.css">
     </head>
 
     <body>
         <header>
-            <div class="container">
+            <div class="container bd-dark mb-5">
                 <h1 class="page-title">My Team</h1>
             </div>
         </header>
 
         <main class="container">
-            
+            <div class="row>
+                <div class="col-6>
+                </div>
+            </div>           
         </main>
     </body>
     </html>
     `;
 }
+
+module.exports = generatePage;
